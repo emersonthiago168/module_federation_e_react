78486a65849442b4f78430811f5d444ec4c42220
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-12-11 15:31:17 -0300

    criando o aplicativo pai

---
+++ packages/app/src/App.js
@@ -0,0 +1,14 @@
+import React, { Suspense } from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link 
+} from 'react-router-dom';
+import {
+    Navbar,
+    Nav,
+    NavItem
+} from 'reactstrap'
+
+// CSS
