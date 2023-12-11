4995c6c0e61a86e9b0ce80e13775c7a34324fdcf
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-12-11 18:34:07 -0300

    unindo todos os aplicativos

--- packages/app/src/App.js
+++ packages/app/src/App.js
@@ -1,7 +1,7 @@
 import React, { Suspense } from 'react';
 import {
     BrowserRouter as Router,
-    Switch,
+    Routes,
     Route,
     Link
 } from 'react-router-dom';
@@ -17,9 +17,11 @@ import './app.css';
 const App = () => {
 
     // MF
+    const HomePage = React.lazy(() => import("HomeApp/HomePage"));
+    const ContactPage = React.lazy(() => import("ContactApp/ContactPage"));
 
     return (
-        < Router >
+        <Router>
             <div>
                 <Navbar color="light" light expand="md">
                     <Nav className='mr-auto' navbar>
@@ -31,8 +33,20 @@ const App = () => {
                         </NavItem>
                     </Nav>
                 </Navbar>
+                <Routes>
+                    <Route exact path="/">
+                        <Suspense fallback="Loading Home Page...">
+                            <HomePage />
+                        </Suspense>
+                    </Route>
+                    <Route exact path="/contact">
+                        <Suspense fallback="Loading Contact Page...">
+                            <ContactPage />
+                        </Suspense>
+                    </Route>
+                </Routes>
             </div>
-        </Router >
+        </Router>
     )
 }
 
